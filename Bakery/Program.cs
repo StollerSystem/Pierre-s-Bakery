ffd256945bd182135384ad96fa20f9224f3203f5
Ben Stoller <stollersystemsound@gmail.com>
2020-09-25 11:57:31 -0700

    add 3 bread and 3 pastry options to the menu and option to clear cart

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -14,6 +14,15 @@ namespace Bakery
       Console.WriteLine("\nPress any key to continue...");
       Console.ReadLine();
       // LOOP HERE AND SHOW CART 
+      Console.WriteLine("Type the number, then hit enter to add the item to your cart");
+      Console.WriteLine("'1' to add Sour Dough Bread");
+      Console.WriteLine("'2' to add Wheat Bread");
+      Console.WriteLine("'3' to add Rye Bread");
+      Console.WriteLine("'4' to add Scone Pastry");
+      Console.WriteLine("'5' to add Muffin Pastry");
+      Console.WriteLine("'6' to add Croissant Pastry");
+      Console.WriteLine("Type 'c' to clear your cart!");
+      Console.WriteLine("Type 'd' when you're done!");
       bool shopping = true;
       while (shopping)
       {
@@ -34,36 +43,55 @@ namespace Bakery
           {
             Console.WriteLine("-"+pastry.Description);
           }
-        }
-
+        }        
+        // ORDER FORM
         
-
-        Console.WriteLine("\n'sd' to add Sour Dough Bread");
         string userInput = Console.ReadLine().ToLower();
 
         switch (userInput)
         {
-          case ("sd"):
-            // ADD SOUR DOUGH
+          case ("1"):
             Bread sourDough = new Bread("Sour Dough");
             ShoppingCart.AddBread(sourDough);
             Console.WriteLine("Sour dough added!");
             break;
-
-          case ("mf"):
-            // ADD SOUR DOUGH
+          case ("2"):
+            Bread wheat = new Bread("Wheat");
+            ShoppingCart.AddBread(wheat);
+            Console.WriteLine("Wheat added!");
+            break;
+          case ("3"):
+            Bread rye = new Bread("Rye");
+            ShoppingCart.AddBread(rye);
+            Console.WriteLine("Rye added!");
+            break;
+          case ("4"):
+            Pastry scone = new Pastry("Scone");
+            ShoppingCart.AddPastry(scone);
+            Console.WriteLine("Scone added!");
+            break;
+          case ("5"):
             Pastry muffin = new Pastry("Muffin");
             ShoppingCart.AddPastry(muffin);
             Console.WriteLine("Muffin added!");
             break;
-
+          case ("6"):
+            Pastry croissant = new Pastry("Croissant");
+            ShoppingCart.AddPastry(croissant);
+            Console.WriteLine("Croissant added!");
+            break;
+          case ("c"):
+            //CLEAR
+            Console.WriteLine("Cart cleared!");
+            ShoppingCart.ClearAll();
+            break;
           case ("d"):
             //DONE
             Console.WriteLine("done!");
             shopping = false;
             break;
           default:
-            Console.WriteLine("what?");
+            Console.WriteLine("Sorry please try again...type the NUMBER of the item you want.");
             break;
         } 
       }
