0415b2c32361dae1bd1c13db84a03ca07566f670
Ben Stoller <stollersystemsound@gmail.com>
2020-09-25 13:00:11 -0700

    add initial styling to the text UI interface

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -6,46 +6,23 @@ namespace Bakery
   public class Program
   {
     public static void Main() 
-    {
-      Console.WriteLine("\n*** Welcome to Pierre's Bakery! ***");
-      Console.WriteLine("\nOur prices:");
-      Console.WriteLine("Bread: Buy 2 get 1 free! Single loaf $5");
+    { 
+      Console.WriteLine("\n----------------------------------------");
+      Console.WriteLine("|  *** Welcome to Pierre's Bakery ***  |");
+      Console.WriteLine("----------------------------------------");
+      Console.WriteLine("\nOur Prices:");
+      Console.WriteLine("Bread: Single loaf $5 Buy 2 get 1 free!");
       Console.WriteLine("Pastry: Buy 1 for $2 or 3 for $5!");
       Console.WriteLine("\nPress any key to continue...");
       Console.ReadLine();
       // LOOP HERE AND SHOW CART 
-      Console.WriteLine("Type the number, then hit enter to add the item to your cart");
-      Console.WriteLine("'1' to add Sour Dough Bread");
-      Console.WriteLine("'2' to add Wheat Bread");
-      Console.WriteLine("'3' to add Rye Bread");
-      Console.WriteLine("'4' to add Scone Pastry");
-      Console.WriteLine("'5' to add Muffin Pastry");
-      Console.WriteLine("'6' to add Croissant Pastry");
-      Console.WriteLine("Type 'c' to clear your cart!");
-      Console.WriteLine("Type 'd' when you're done!");
+      PrintMenu();
       bool shopping = true;
       while (shopping)
       {
         // print cart here
-        Console.WriteLine("\nShpping cart: ");
-        List<Bread> breadCart = ShoppingCart.GetBread();
-        List<Pastry> pastryCart = ShoppingCart.GetPastry();
-        if (breadCart.Count == 0 && pastryCart.Count == 0)
-        {
-          Console.WriteLine("-Empty!");
-        } else 
-        {
-            foreach (Bread bread in breadCart)
-          {
-            Console.WriteLine("-"+bread.Description);
-          }          
-          foreach (Pastry pastry in pastryCart)
-          {
-            Console.WriteLine("-"+pastry.Description);
-          }
-        }        
-        // ORDER FORM
-        
+       PrintCart();    
+        // ORDER FORM        
         string userInput = Console.ReadLine().ToLower();
 
         switch (userInput)
@@ -53,37 +30,41 @@ namespace Bakery
           case ("1"):
             Bread sourDough = new Bread("Sour Dough");
             ShoppingCart.AddBread(sourDough);
-            Console.WriteLine("Sour dough added!");
+            Console.WriteLine("\n*** Sour dough added! ***");
             break;
           case ("2"):
             Bread wheat = new Bread("Wheat");
             ShoppingCart.AddBread(wheat);
-            Console.WriteLine("Wheat added!");
+            Console.WriteLine("\n*** Wheat added! ***");
             break;
           case ("3"):
             Bread rye = new Bread("Rye");
             ShoppingCart.AddBread(rye);
-            Console.WriteLine("Rye added!");
+            Console.WriteLine("\n*** Rye added! ***");
             break;
           case ("4"):
             Pastry scone = new Pastry("Scone");
             ShoppingCart.AddPastry(scone);
-            Console.WriteLine("Scone added!");
+            Console.WriteLine("\n*** Scone added! ***");
             break;
           case ("5"):
             Pastry muffin = new Pastry("Muffin");
             ShoppingCart.AddPastry(muffin);
-            Console.WriteLine("Muffin added!");
+            Console.WriteLine("\n*** Muffin added! ***");
             break;
           case ("6"):
             Pastry croissant = new Pastry("Croissant");
             ShoppingCart.AddPastry(croissant);
-            Console.WriteLine("Croissant added!");
+            Console.WriteLine("\n*** Croissant added! ***");
             break;
           case ("c"):
             //CLEAR
-            Console.WriteLine("Cart cleared!");
+            Console.WriteLine("\n*** Cart cleared! ***");
             ShoppingCart.ClearAll();
+            break;
+           case ("m"):
+            //Menu
+            PrintMenu();            
             break;
           case ("d"):
             //DONE
@@ -91,10 +72,49 @@ namespace Bakery
             shopping = false;
             break;
           default:
-            Console.WriteLine("Sorry please try again...type the NUMBER of the item you want.");
+            Console.WriteLine("Sorry please try again...type the NUMBER of the item you want. Type 'm' to see the menu.");
             break;
         } 
+      }      
+    }
+    public static void PrintMenu()
+      {
+        Console.WriteLine("\n----------------------");
+        Console.WriteLine("Type the number, then hit enter to add the item to your cart");
+        Console.WriteLine("'1' to add Sour Dough Bread");
+        Console.WriteLine("'2' to add Wheat Bread");
+        Console.WriteLine("'3' to add Rye Bread");
+        Console.WriteLine("'4' to add Scone Pastry");
+        Console.WriteLine("'5' to add Muffin Pastry");
+        Console.WriteLine("'6' to add Croissant Pastry");
+        Console.WriteLine("Type 'c' to clear your cart!");
+        Console.WriteLine("Type 'm' to see the menu!");
+        Console.WriteLine("Type 'd' when you're done!");
+        Console.WriteLine("----------------------");
       }
+
+    public static void PrintCart()
+    {
+      Console.WriteLine("\n----------------------");
+      Console.WriteLine("Shpping Cart: ");
+      List<Bread> breadCart = ShoppingCart.GetBread();
+      List<Pastry> pastryCart = ShoppingCart.GetPastry();
+      if (breadCart.Count == 0 && pastryCart.Count == 0)
+      {
+        Console.WriteLine(" -Empty!");
+      } else 
+      {
+          foreach (Bread bread in breadCart)
+        {
+          Console.WriteLine(" -"+bread.Description);
+        }          
+        foreach (Pastry pastry in pastryCart)
+        {
+          Console.WriteLine(" -"+pastry.Description);
+        }
+      }
+      Console.WriteLine("----------------------"); 
+      Console.WriteLine("(Type 'm' to see the menu)");
     }
   }
 }
