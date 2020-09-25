c1d9ec841c3730e01cd760a8c2bac94e0b62cc4a
Ben Stoller <stollersystemsound@gmail.com>
2020-09-25 10:21:56 -0700

    start building initial console UI

---
+++ Bakery/Program.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Bakery
+{   
+  public class Program
+  {
+    public static void Main() 
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("Our prices:");
+      Console.WriteLine("Bread: Buy 2 get 1 free! Single loaf $5");
+      Console.WriteLine("Pastry: Buy 1for $2 or 3 for $5!");
+      Console.WriteLine("Press any key to continue");
+      Console.ReadLine();
+
+    }
+  }
+}
